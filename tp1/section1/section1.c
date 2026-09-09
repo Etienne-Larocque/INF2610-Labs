5156be4d2d77c049262001370a3eb9deba03550a
Etienne-Larocque <etienne-2.larocque@etud.polymtl.ca>
2026-09-09 09:45:57 -0400

    Depot init lab1

---
+++ tp1/section1/section1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <mots_file>\n", argv[0]);
+        return 1;
+    }
+
+
+    return 0;
+}
